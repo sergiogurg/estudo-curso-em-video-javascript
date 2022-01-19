4a0b816d82a9802b9ac3df872dce9fdc6a077eab
sergiogurgel <sergiogurgell@gmail.com>
2022-01-19 11:16:16 -0300

    exercício js 009
    Estrutura condicional. Condição Composta (else).

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Se você nasceu em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é brasileiro(a)(e)')
+} else{
+    console.log('Você é estrangeiro(a)(e)')
+}
