c04b6ab8200912477d132fd6dc32307e30037dbb
sergiogurgel <sergiogurgell@gmail.com>
2022-01-20 14:38:44 -0300

    prática js aula 14
    Repetição/Loop/Laços. Estrutura de repetição com contador lógico (for).

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+// Loop com while
+
+/*  
+var cont = 1
+do {
+    console.log(`Cont = ${cont}`)
+    cont++
+} while (cont <= 5)
+
+*/
+
+//-------------------------
+// Loop com for
+
+for (cont = 1; cont <= 5; cont++) {
+    console.log(`Cont = ${cont}`)
+}
